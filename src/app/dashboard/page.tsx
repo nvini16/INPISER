5ada219cd74dcd9c38e85dbdddcebb904b660b3f
Nathanael <nathanael.vinicius03@gmail.com>
2026-08-29 22:18:13 -0300

    fix: corrigir detalhes do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,11 @@
-import Sidebar from '../componentes/Sidebar';
+import Sidebar from "../componentes/Sidebar";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex bg-black text-white">
-      
-      {/* Valor de prop {titulo} criado em Sidebar.tsx */}
+    <main className="flex min-h-screen bg-black text-white">
       <Sidebar titulo="Instagram Inpiser" />
 
-
       <section className="flex-1 p-8">
-
-        {/* Div de descrição */}
         <div>
           <h2 className="text-3xl font-bold">Dashboard</h2>
           <p className="mt-2 text-gray-400">
@@ -17,9 +13,8 @@ export default function Home() {
           </p>
         </div>
 
-       {/* Dashboards */}
+        {/* Dashboards */}
         <div className="mt-8 flex flex-col gap-4 md:flex-row">
-
           {/* Card 1 */}
           <div className="flex flex-1 flex-col gap-2 rounded border border-green-900 bg-gray-900 p-4">
             <h3 className="text-sm text-gray-400">Publicações</h3>
@@ -42,22 +37,20 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Ações rpidas */}
+        {/* Ações rápidas */}
         <div className="mt-8 flex items-center justify-between">
           <h2 className="text-2xl font-bold">Ações rápidas</h2>
 
           {/* Botões de ação rápida */}
           <div className="flex gap-2">
-
             <button className="rounded bg-green-500 px-4 py-2 font-medium text-white hover:bg-green-600">
               Criar publicação
             </button>
 
-            <button className="bg-gray-800 px-4 py-2 rounded borde border-green-900 text-white hover:bg-green-900">
+            <button className="rounded border border-green-900 bg-gray-800 px-4 py-2 text-white hover:bg-green-900">
               Agendar publicações
             </button>
           </div>
-
         </div>
       </section>
     </main>
